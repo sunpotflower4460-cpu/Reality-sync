b7398c52a98419d7689e3f1c21a7bc412154e49e
sunpotflower4460-cpu <sunpotflower4460@gmail.com>
2026-08-24 11:46:45 +0900

    Add release metadata constants

---
+++ src/config/app.js
@@ -0,0 +1,6 @@
+export const APP_NAME = 'RealitySync';
+export const APP_VERSION = '1.0.0';
+export const SUPPORT_URL = 'https://github.com/sunpotflower4460-cpu/Reality-sync/issues';
+export const PRIVACY_POLICY_URL = 'https://sunpotflower4460-cpu.github.io/Reality-sync/privacy.html';
+export const TERMS_URL = 'https://sunpotflower4460-cpu.github.io/Reality-sync/terms.html';
+export const SUPPORT_PAGE_URL = 'https://sunpotflower4460-cpu.github.io/Reality-sync/support.html';
