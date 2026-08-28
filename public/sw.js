5c4ae2d2b55ca28b76bf12c0aa4885f149282b30
sunpotflower4460-cpu <sunpotflower4460@gmail.com>
2026-08-28 18:45:43 +0900

    Scope service worker cache cleanup to RealitySync caches

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,5 @@
-const CACHE_NAME = 'reality-sync-shell-v2';
+const CACHE_PREFIX = 'reality-sync-shell-';
+const CACHE_NAME = `${CACHE_PREFIX}v3`;
 const MAX_RUNTIME_ASSET_ENTRIES = 24;
 const scopeUrl = new URL(self.registration.scope);
 const indexUrl = new URL('index.html', scopeUrl);
@@ -52,7 +53,9 @@ self.addEventListener('install', (event) => {
 self.addEventListener('activate', (event) => {
   event.waitUntil(
     caches.keys()
-      .then((keys) => Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))))
+      .then((keys) => Promise.all(keys
+        .filter((key) => key.startsWith(CACHE_PREFIX) && key !== CACHE_NAME)
+        .map((key) => caches.delete(key))))
       .then(() => self.clients.claim()),
   );
 });
